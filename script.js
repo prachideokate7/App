089690adfd9579803582e06a945cb5c9e86f0db0
Gcekweb <prachideokate2001@gmail.com>
2021-06-14 23:11:46 +0530

    Home page without contact form and footer

---
+++ script.js
@@ -0,0 +1,11 @@
+/*Menu javascript*/
+
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+}
+
+/*Menu javascript completed*/
